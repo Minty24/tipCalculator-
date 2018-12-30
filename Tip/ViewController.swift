7d2a92bed8ab1970c271d3e86183baf2517b3b5d
Min Zheng <Minty-Min@mindemacbook-air.local>
2018-12-30 14:37:03 -0500

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Min Zheng on 12/30/18.
+//  Copyright © 2018 Min Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
